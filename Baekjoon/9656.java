86bad222ae284b48d3666a252bd6101ac56d33eb
SungJae Kim <ksj0495@naver.com>
2019-09-06 22:36:17 +0900

    [CREATE] 9656.java
    백준 돌 게임2

---
+++ Baekjoon/9656.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+		
+	public static void main(String args[]) throws IOException {
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(input.readLine());
+		
+		System.out.println((N & 1)== 0 ? "SK" : "CY");
+	}
+}
