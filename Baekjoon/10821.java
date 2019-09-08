5e6761bc2df21546972130418dd718a3ddfdb791
SungJae Kim <ksj0495@naver.com>
2019-09-08 22:56:24 +0900

    [CREATE] 10821.java
    백준 정수의 개수

---
+++ Baekjoon/10821.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String args[]) throws Exception{
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		String tc[] = input.readLine().split(",");
+		System.out.println(tc.length);
+	}
+}
