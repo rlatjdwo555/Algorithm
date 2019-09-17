3a2ee7c619fcbcf5c2cadf5b496573b7bd37465f
SungJae Kim <ksj0495@naver.com>
2019-09-17 16:45:11 +0900

    [CREATE] 11654.java
    백준 아스키코드

---
+++ Baekjoon/11654.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String args[]) throws Exception{
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println((int)input.readLine().charAt(0));
+	}
+}
