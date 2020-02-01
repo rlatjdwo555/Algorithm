cf5173ac01194f97d97d608e57f71f6869350107
ksj0495 <ksj0495@naver.com>
2020-02-01 23:18:33 +0900

    [CREATE] 42588.java
    탑

---
+++ Programmers/42588.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+class Solution {
+    public int[] solution(int[] heights) {
+		int answer[] = new int[heights.length];
+		
+		for(int i=0; i<heights.length; i++){
+			for(int j=i+1; j<heights.length; j++){
+				if(heights[i] > heights[j]){
+					answer[j] = i+1;
+				}
+			}
+		}
+		
+		return answer;
+    }
+}
